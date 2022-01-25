a4c36992decde6442846841a2b06da3e0c4cfd34
Heidi <hlmunson@hotmail.com>
2022-01-24 17:08:27 -0700

    update get_outputs and comments

--- Hilo/game/dealer.py
+++ Hilo/game/dealer.py
@@ -1,32 +1,33 @@
+
 from game.card import Card
 
 
 class Dealer:
     """A person who directs the game. 
     
-    The responsibility of a Director is to control the sequence of play.
+    The responsibility of a Dealer is to control the sequence of play.
 
     Attributes:
-        dice (List[Die]): A list of Die instances.
+        deck (List[deck]): A list of Card instances.
         is_playing (boolean): Whether or not the game is being played.
         score (int): The score for one round of play.
         total_score (int): The score for the entire game.
     """
 
     def __init__(self):
-        """Constructs a new Director.
+        """Constructs a new Dealer.
         
         Args:
-            self (Director): an instance of Director.
+            self (Dealer): an instance of Dealer.
         """
-        self.dice = []
+        self.deck = []
         self.is_playing = True
         self.score = 0
         self.total_score = 0
 
-        for i in range(5):
-            die = Die()
-            self.dice.append(die)
+        for i in range(13):
+            card = Card()
+            self.deck.append(card)
 
     def start_game(self):
         """Starts the game by running the main game loop.
@@ -40,13 +41,13 @@ class Dealer:
             self.do_outputs()
 
     def get_inputs(self):
-        """Ask the user if they want to roll.
+        """Ask the user if they want to a new card.
 
         Args:
-            self (Director): An instance of Director.
+            self (Dealer): An instance of Dealer.
         """
-        roll_dice = input("Roll dice? [y/n] ")
-        self.is_playing = (roll_dice == "y")
+        hi_lo = input("Higher or Lower? [h/l] ")
+        self.is_playing = (hi_lo == "y")
        
     def do_updates(self):
         """Updates the player's score.
@@ -57,26 +58,29 @@ class Dealer:
         if not self.is_playing:
             return 
 
-        for i in range(len(self.dice)):
-            die = self.dice[i]
-            die.roll()
-            self.score += die.points 
+        for i in range(len(self.deck)):
+            card = self.deck[i]
+            card.roll()
+            self.score += card.points 
         self.total_score += self.score
 
     def do_outputs(self):
-        """Displays the dice and the score. Also asks the player if they want to roll again. 
+        """Displays the card and the score. Also asks the player if they want to play again. 
 
         Args:
-            self (Director): An instance of Director.
+            self (Dealer): An instance of Dealer.
         """
         if not self.is_playing:
             return
-        
-        values = ""
-        for i in range(len(self.dice)):
-            die = self.dice[i]
-            values += f"{die.value} "
 
-        print(f"You rolled: {values}")
+        values = ""
+        for i in range(len(self.deck)):
+            card = self.deck[i]
+            values += f"{card.value} "
+
+        print(f"The card is: {values}")
         print(f"Your score is: {self.total_score}\n")
         self.is_playing == (self.score > 0)
+        play_again = input("Would you like to play again? (y/n) ")
+        self.is_playing = (play_again == "y")
+        
